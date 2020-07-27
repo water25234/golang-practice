6c54a69fe776b6a61cd059d69aff6f1216bfbf7b
water25234 <water25234@gmail.com>
2020-07-27 12:47:20 +0800

    add singleton

---
+++ singleton/main.go
@@ -0,0 +1,41 @@
+// https://www.twblogs.net/a/5db39e0cbd9eee310ee6a098
+// https://www.jianshu.com/p/a21d17fc5598
+// http://researchlab.github.io/2016/02/25/singleton-pattern-in-go/
+
+package main
+
+import "fmt"
+
+type singleton struct {
+	count int
+}
+
+var instance *singleton
+
+func GetInstance() *singleton {
+	if instance == nil {
+		instance = new(singleton)
+	}
+	return instance
+}
+
+func (s *singleton) Addone() int {
+	s.count++
+	return s.count
+}
+
+func main() {
+	getInstance1 := GetInstance()
+	addone1 := getInstance1.Addone()
+	fmt.Println(getInstance1.count)
+	fmt.Println(addone1)
+
+	getInstance2 := GetInstance()
+	addone2 := getInstance2.Addone()
+	fmt.Println(getInstance2.count)
+	fmt.Println(addone2)
+
+	if addone1 != addone2 {
+		fmt.Println("instance is not equal。")
+	}
+}
