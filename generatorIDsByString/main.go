15f60b81b96833f65c0b5a235dc710787ecfb4e1
JustinHuangNewType <Justin.huang@newtype.games>
2020-09-08 12:05:45 +0800

    add fnv32

--- generatorIDsByString/main.go
+++ generatorIDsByString/main.go
@@ -8,7 +8,8 @@ import (
 
 func main() {
 
-	execStringByID()
+	// execStringByID()
+	execStringByID_fnv32()
 
 	// fmt.Println(GetCheckIDs("8TtAq4imIgLOaJGQZEbpmbEtTRLG14"))
 	// fmt.Println(GetCheckIDs("NLZR2KgcRyqfxj8n9Zj77nl8uGlrLN"))
@@ -34,6 +35,20 @@ func execStringByID() {
 		}
 		arr[CheckIDs] = str
 		fmt.Println(CheckIDs, arr[CheckIDs], str)
+	}
+}
+
+func execStringByID_fnv32() {
+	arr := make(map[uint32]string)
+	for i := 0; i < 100000; i++ {
+		str := String(20)
+		CheckIDs := fnv32(str)
+		if _, ok := arr[CheckIDs]; ok {
+			if arr[CheckIDs] != str {
+				fmt.Println(CheckIDs, arr[CheckIDs], str)
+			}
+		}
+		arr[CheckIDs] = str
 	}
 }
 
@@ -73,71 +88,17 @@ func WithBranch(n int64) int64 {
 	return n
 }
 
-var (
-	Field_mapping = map[string]int64{
-		"0": 0,
-		"1": 1,
-		"2": 2,
-		"3": 3,
-		"4": 4,
-		"5": 5,
-		"6": 6,
-		"7": 7,
-		"8": 8,
-		"9": 9,
-		"A": 10,
-		"B": 11,
-		"C": 12,
-		"D": 13,
-		"E": 14,
-		"F": 15,
-		"G": 16,
-		"H": 17,
-		"I": 18,
-		"J": 19,
-		"K": 20,
-		"L": 21,
-		"M": 22,
-		"N": 23,
-		"O": 24,
-		"P": 25,
-		"Q": 26,
-		"R": 27,
-		"S": 28,
-		"T": 29,
-		"U": 30,
-		"V": 31,
-		"X": 32,
-		"Y": 33,
-		"Z": 34,
-		"a": 35,
-		"b": 36,
-		"c": 37,
-		"d": 38,
-		"e": 39,
-		"f": 40,
-		"g": 41,
-		"h": 42,
-		"i": 43,
-		"j": 44,
-		"k": 45,
-		"l": 46,
-		"m": 47,
-		"n": 48,
-		"o": 49,
-		"p": 50,
-		"q": 51,
-		"r": 52,
-		"s": 53,
-		"t": 54,
-		"u": 55,
-		"v": 56,
-		"w": 57,
-		"x": 58,
-		"y": 59,
-		"z": 60,
+// https://golang.org/src/hash/fnv/fnv.go
+// https://www.timiguo.com/archives/176/
+func fnv32(key string) uint32 {
+	hash := uint32(2166136261)
+	const prime32 = uint32(16777619)
+	for i := 0; i < len(key); i++ {
+		hash *= prime32
+		hash ^= uint32(key[i])
 	}
-)
+	return hash
+}
 
 const charset = "abcdefghijklmnopqrstuvwxyz" +
 	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -155,53 +116,4 @@ func StringWithCharset(length int, charset string) string {
 
 func String(length int) string {
 	return StringWithCharset(length, charset)
-}
-
-func Arr() []string {
-	arr := []string{
-		"",
-		"X",
-		"XX",
-		"XXX",
-		"XXXX",
-		"XXXXX",
-		"XXXXXX",
-		"XXXXXXX",
-		"XXXXXXXX",
-		"XXXXXXXXX",
-		"XXXXXXXXXX",
-		"XXXXXXXXXXX",
-		"XXXXXXXXXXXX",
-		"XXXXXXXXXXXXX",
-		"XXXXXXXXXXXXXX",
-		"XXXXXXXXXXXXXXX",
-		"XXXXXXXXXXXXXXXX", // 0
-		"XXXXXXXXXXXXXXXXX",
-		"XXXXXXXXXXXXXXXXXX",
-		"XXXXXXXXXXXXXXXXXXX",
-		"justin.huang@kkday.com",
-		"justin.huang@NewType.games",
-		"water25234@gmail.com",
-		"water25234@hotmail.com",
-		"abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com",
-		"",
-		"A",
-		"AA",
-		"AAA",
-		"AAAA",
-		"AAAAA",
-		"AAAAAA",
-		"AAAAAAA",
-		"AAAAAAAA",
-		"AAAAAAAAA",
-		"AAAAAAAAAA",
-		"AAAAAAAAAAA",
-		"AAAAAAAAAAAA",
-		"AAAAAAAAAAAAA",
-		"AAAAAAAAAAAAAA",
-		"AAAAAAAAAAAAAAA",
-		"AAAAAAAAAAAAAAAA",
-		"AAAAAAAAAAAAAAAAA",
-	}
-	return arr
 }
