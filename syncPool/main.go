9125708779597f2e40ba63fb5a04b580d1481447
water25234 <water25234@gmail.com>
2021-01-24 23:08:54 +0800

    add sync pool

---
+++ syncPool/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+
+	pool := &sync.Pool{
+
+		New: func() interface{} {
+			return 0
+		},
+	}
+
+	init := pool.Get()
+	fmt.Println(init)
+
+	// 設定參數
+	pool.Put(1)
+
+	// 取得結果
+	num := pool.Get()
+	fmt.Println(num)
+
+	// 在取得一次是取得default 0
+	num = pool.Get()
+	fmt.Println(num)
+}
